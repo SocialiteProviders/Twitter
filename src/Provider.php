a225c81437f2876484d2870e21f3e3508f04f1d7
DraperStudio <hello@draperstudio.tech>
2016-03-31 04:32:58 -0400

    Applied fixes from StyleCI

--- src/Provider.php
+++ src/Provider.php
@@ -5,7 +5,7 @@ namespace SocialiteProviders\Twitter;
 use SocialiteProviders\Manager\OAuth1\User;
 use SocialiteProviders\Manager\OAuth1\AbstractProvider;
 
-class Provider extends  AbstractProvider
+class Provider extends AbstractProvider
 {
     /**
      * Unique Provider Identifier.
